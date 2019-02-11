62bcadb9570cc07bdb9f962ef76d0982b6ffcf43
roharon <roharon98@gmail.com>
2019-02-11 22:56:55 +0900

    fix: console.log
    불필요 로그 삭제

--- func/test.js
+++ func/test.js
@@ -1,3 +1,16 @@
 const cafe_crawl = require('./cafe_crawl')
+const request = require('sync-request')
+var temp = null
 
-console.log(cafe_crawl('inmoon', 'today'))
+const url = 'http://myserver.aaronroh.org:5000/cafe/inmoon/today'
+
+// test code
+var response = request('GET', url)
+var result = response.getBody('utf-8')
+result = JSON.parse(result)
+console.log(result)
+console.log('\n\n', result['title'])
+console.log(Object.keys(result['title']).length)
+var a = ['a']
+
+console.log(a*10)
